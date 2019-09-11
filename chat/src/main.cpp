c4829a97d873db9f6c822001be90fdabe965574b
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-11 17:01:35 -0300

    teste

--- chat/src/main.cpp
+++ chat/src/main.cpp
@@ -88,14 +88,14 @@ int main(int argc, char const *argv[]) {
           // Creating socket file descriptor
           if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
             perror("socket failed");
-            exit(EXIT_FAILURE);
+            //exit(EXIT_FAILURE);
           }
 
           // Forcefully attaching socket to the port 8080
           if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                          &opt, sizeof(opt))) {
             perror("setsockopt");
-            exit(EXIT_FAILURE);
+            //exit(EXIT_FAILURE);
           }
           address.sin_family = AF_INET;
           address.sin_addr.s_addr = INADDR_ANY;
@@ -105,16 +105,16 @@ int main(int argc, char const *argv[]) {
           if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) <
               0) {
             perror("bind failed");
-            exit(EXIT_FAILURE);
+            //exit(EXIT_FAILURE);
           }
           if (listen(server_fd, 3) < 0) {
             perror("listen");
-            exit(EXIT_FAILURE);
+            //exit(EXIT_FAILURE);
           }
           if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                                    (socklen_t *)&addrlen)) < 0) {
             perror("accept");
-            exit(EXIT_FAILURE);
+            //exit(EXIT_FAILURE);
           }
           std::cout  << "aqui" << std::endl;
           valread = read(new_socket, buffer, 1024);
