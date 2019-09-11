cecabcbe792f52810bca74303dcb4602bfa39f60
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-11 14:17:03 -0300

    main ok

--- S-DES/src/main.cpp
+++ S-DES/src/main.cpp
@@ -17,14 +17,45 @@
 #include "SaveBinaryFile.h"
 #include "TextToBinary.h"
 
-#include <iostream>
-#include <string>  // std::string
+#include <iostream>  // std::cout , std::endl
+#include <string>    // std::string
 
 void codificar(std::string);    // Codifiar texto
 void decodificar(std::string);  // Decodificar texto binário
 
+/**
+ * @brief Função menu
+ *
+ * @param argc Quantidade de parãmetros recebidos
+ * @param argv Parâmetros recebidos
+ * @return int 0 WARNING Programa não execultou
+ * @return int 1 ERROR Argumntos não digitados corretamente
+ * @return int 2 ERROR Opção inválida
+ * @return int 3 SUCCESS Arquivo codificado
+ * @return int 4 SUCCESS Arquivo decodificado
+ */
 int main(int argc, char const *argv[]) {
-  /* code */
+  if (argc != 3) {
+    std::cout << "Erro! Digitar os argumentos corretamente." << std::endl;
+    return 1;
+  } else {
+    std::string opcao = argv[1];
+    std::string caminho = argv[2];
+    if (opcao == "codificar") {
+      codificar(caminho);
+      std::cout << "Arquivo " << caminho << " codificado !" << std::endl;
+      return 3;
+    } else {
+      if (opcao == "decodificar") {
+        decodificar(caminho);
+        std::cout << "Arquivo " << caminho << " decodificado !" << std::endl;
+        return 4;
+      } else {
+        std::cout << "Erro! Digite uma opção válida" << std::endl;
+        return 2;
+      }
+    }
+  }
   return 0;
 }
 
