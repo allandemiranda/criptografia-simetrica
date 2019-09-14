2febb60fedf9284f640418f8c1487d7ecc82192d
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-14 14:06:30 -0300

    gerador de keys ok

--- S-DES/include/KeyGenerationDes.h
+++ S-DES/include/KeyGenerationDes.h
@@ -12,99 +12,26 @@
 #ifndef KEYGENERATIONDES_H_
 #define KEYGENERATIONDES_H_
 
-#include <omp.h>   // OpenMP
-#include <bitset>  // std::bitset
+#include <string>  // std::string
 #include <vector>  // std::vector
 
 class KeyGenerationDes {
  private:
-  char keyMaster;                                     // Chave principal
-  std::vector<char> keys;                             // Chaves geradas
-  char getKeyMaster(void);                            // Obter chave principal
-  void setKeyMaster(char);                            // Definir chave principal
-  void setSubKey(char);                               // Definir sub chave
-  void generateSubKeys(char);                         // Gerar as sub chaves
-  char permutation(char, std::vector<unsigned int>);  // Permutação de chave
+  std::string keyMaster;   // Chave principal
+  std::vector<char> keys;  // Chaves geradas
+  void setSubKey(char);    // Definir sub chave
+  std::string getPermutation(std::string,
+                             std::vector<unsigned int>);  // Permutação de chave
   std::vector<unsigned int> P10 = {3, 5, 2, 7, 4, 10, 1, 9, 8, 6};  // P10
   std::vector<unsigned int> P8 = {6, 3, 7, 4, 8, 5, 10, 9};         // P8
-  char LS(char, unsigned int);                                      // LS
-  char keyRight(char);  // Parte direita da chave
-  char keyLeft(char);   // Parte esquerda da chave
+  std::string getLS(std::string, unsigned int);                     // LS
+  std::string getKeyRight(std::string);  // Parte direita da chave em string
+  std::string getKeyLeft(std::string);   // Parte esquerda da chave em string
 
  public:
   char getSubKey(unsigned int);  // Ober sub chave
-  KeyGenerationDes(char);
+  KeyGenerationDes(std::string);
   ~KeyGenerationDes(void);
 };
-
-/**
- * @brief Construct a new Key Generation Des:: Key Generation Des object
- *
- * @param key_master Chave principal
- */
-KeyGenerationDes::KeyGenerationDes(char key_master) {}
-
-/**
- * @brief Destroy the Key Generation Des:: Key Generation Des object
- *
- */
-KeyGenerationDes::~KeyGenerationDes(void) {}
-
-/**
- * @brief Get the Key Master object
- *
- * @return char Chave principal
- */
-char KeyGenerationDes::getKeyMaster(void) { return keyMaster; }
-
-/**
- * @brief Set the Key Master object
- *
- * @param key_master Chave princial
- */
-void KeyGenerationDes::setKeyMaster(char key_master) { keyMaster = key_master; }
-
-/**
- * @brief Set the Sub Key object
- *
- * @param sub_key Sub chave
- */
-void KeyGenerationDes::setSubKey(char sub_key) { keys.push_back(sub_key); }
-
-/**
- * @brief Gerar as sub chaves
- *
- * @param key_master Chave principal
- */
-void KeyGenerationDes::generateSubKeys(char key_master) { /** FAZER **/
-}
-
-/**
- * @brief Permutar a chave
- *
- * @param key Chave
- * @param feistel Sequência de permutação
- * @return char Chave permutada
- */
-char KeyGenerationDes::permutation(char key,
-                                   std::vector<unsigned int> feistel) {
-  for (unsigned int i = 0; i < feistel.size(); ++i) {
-    if (feistel[i] > 8) {
-      throw "Vetor Feistel contem elemento maior que tamanho da chave";
-    }
-    if (feistel[i] == 0) {
-      throw "Vetor Feistel contem elemento menor que tamanho da chave";
-    }
-  }
-
-  std::string key_string = std::bitset<8>(key).to_string();
-
-  std::string final_Key;
-  for (unsigned int position : feistel) {
-    final_Key.push_back(key_string[position - 1]);
-  }
-
-  return static_cast<char>(std::bitset<8>(key_string).to_ulong());
-}
 
 #endif
